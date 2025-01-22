8c7243293b2cc1793c70cb24f42761bbe786416d
Samuel_Riedel <samuel.riedel@outlook.com>
2025-01-22 09:49:17 +0100

    created_routes

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import "./Navbar.css";
 
 import logo from "../assets/logo.png";
 import cart_icon from "../assets/cart_icon.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
@@ -19,33 +20,41 @@ const Navbar = () => {
             setMenu("shop");
           }}
         >
-          Tienda{menu === "shop" ? <hr /> : <></>}
+          <Link to="/">Shop</Link>
+          {menu === "shop" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("mens");
           }}
         >
-          Muebles{menu === "mens" ? <hr /> : <></>}
+          <Link to="/mens">Mens</Link>
+          {menu === "mens" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("womens");
           }}
         >
-          Ropa{menu === "womens" ? <hr /> : <></>}
+          <Link to="/womens">Women</Link>
+          {menu === "womens" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("kids");
           }}
         >
-          Juguetes{menu === "kids" ? <hr /> : <></>}
+          <Link to="/kids">Kids</Link>
+          {menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="Cart Icon"></img>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="Cart Icon" />
+        </Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
