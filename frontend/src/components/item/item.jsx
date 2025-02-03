d3496ca6904a8402dce5d36ca7d9b7b1653c0769
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-03 09:53:04 +0100

    fixed_props_on_popular_component_file

--- frontend/src/components/item/item.jsx
+++ frontend/src/components/item/item.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./item.css";
+import "./Item.css";
 
 const Item = (props) => {
   return (
@@ -8,7 +8,7 @@ const Item = (props) => {
       <p>{props.name}</p>
       <div className="item-prices">
         <div className="item-price-new">{props.new_price}</div>
-        <div className="item-price-old">{props.old.price}</div>
+        <div className="item-price-old">{props.old_price}</div>
       </div>
     </div>
   );
