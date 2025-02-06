c85d0cf5a100edf1cfc9de43f6a4997cf115c662
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-06 09:17:56 +0100

    Created_input_for_email_on_newsletter

--- frontend/src/components/newsLetter/NewsLetter.jsx
+++ frontend/src/components/newsLetter/NewsLetter.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./NewsLetter.css";
 
 const NewsLetter = () => {
-  return <div className="newsletter"></div>;
+  return (
+    <div className="newsletter">
+      <h1>Get exclusive offers on your email</h1>
+    </div>
+  );
 };
 
 export default NewsLetter;
