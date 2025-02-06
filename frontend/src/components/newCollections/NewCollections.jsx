d943a27080955f7b80b8fa1a7b75d9bdcfaf115f
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-06 09:03:26 +0100

    imported_items_collection_fromitems

--- frontend/src/components/newCollections/NewCollections.jsx
+++ frontend/src/components/newCollections/NewCollections.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import "./NewCollections.css";
+import new_collection from "../assets/new_collections";
+import Item from "../item/Item";
 const NewCollections = () => {
-  return <div className="new-collections"></div>;
+  return (
+    <div className="new-collections">
+      <h1>New Collections</h1>
+      <hr />
+      <div className="collections">
+        {new_collection.map((item, index) => {
+          return <Item />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default NewCollections;
