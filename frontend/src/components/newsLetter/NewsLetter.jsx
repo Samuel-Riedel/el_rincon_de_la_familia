ada4b85eb659dc91092b6ca52b89e3e32c45b8b9
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-06 09:19:04 +0100

    created_button_to_subscribe_in_newsletter_component

--- frontend/src/components/newsLetter/NewsLetter.jsx
+++ frontend/src/components/newsLetter/NewsLetter.jsx
@@ -5,6 +5,11 @@ const NewsLetter = () => {
   return (
     <div className="newsletter">
       <h1>Get exclusive offers on your email</h1>
+      <p>Subscribe to our news letter and stay updated</p>
+      <div>
+        <input type="email" placeholder="Your Email id" />
+        <button>Subscribe</button>
+      </div>
     </div>
   );
 };
