ff17b6a3afe9a19b8288d463a74d6747927a0abe
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-06 08:45:22 +0100

    fixed_offers_classnames_and_css

--- frontend/src/components/offers/Offers.jsx
+++ frontend/src/components/offers/Offers.jsx
@@ -5,13 +5,13 @@ import exclusive_image from "../assets/exclusive_image.png";
 const Offers = () => {
   return (
     <div className="offers">
-      <div className="div-offers-left">
+      <div className="offers-left">
         <h1>Exclusive</h1>
         <h1>Offers for you</h1>
         <p>ONLY ON BEST SELLERS PRODUCTS</p>
         <button>Check Now!</button>
       </div>
-      <div className="div-offers-right">
+      <div className="offers-right">
         <img src={exclusive_image} alt="" />
       </div>
     </div>
