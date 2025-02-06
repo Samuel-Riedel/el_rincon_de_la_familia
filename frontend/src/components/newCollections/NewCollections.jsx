53f71d541cf260ac3f3ecaa19b40f3ca45b14d14
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-06 09:14:08 +0100

    created_newsletter_component_structure

--- frontend/src/components/newCollections/NewCollections.jsx
+++ frontend/src/components/newCollections/NewCollections.jsx
@@ -8,8 +8,17 @@ const NewCollections = () => {
       <h1>New Collections</h1>
       <hr />
       <div className="collections">
-        {new_collection.map((item, index) => {
-          return <Item />;
+        {new_collection.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
         })}
       </div>
     </div>
