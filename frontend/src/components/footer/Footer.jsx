2b79e9307b03405776c5f053e26d89e819369f31
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-07 10:13:10 +0100

    created_copyrights_div_for_footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -29,6 +29,10 @@ const Footer = () => {
           </div>
         </div>
       </div>
+      <div className="footer-copyright">
+        <hr />
+        <p>Copyright @2025 - All Rights Reserved</p>
+      </div>
     </div>
   );
 };
