ef6ce224aa9a43e73346efd707bad21e717c5e8f
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-07 10:02:28 +0100

    created_list_for_footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -7,6 +7,14 @@ const Footer = () => {
     <div className="footer">
       <div className="footer-logo">
         <img src={footer_logo} alt="" />
+        <p>SHOOPER</p>
+        <ul className="footer-links">
+          <li>Company</li>
+          <li>Products</li>
+          <li>Offices</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
       </div>
     </div>
   );
