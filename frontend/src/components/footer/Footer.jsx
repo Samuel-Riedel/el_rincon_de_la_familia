72fad5b3b7a7244016f11ec92b4cd293950e7f7b
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-07 09:57:49 +0100

    imported_footer_logo

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,8 +1,15 @@
 import React from "eact";
 import "./Footer.css";
+import footer_logo from "../assets/logo_big.png";
 
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <div className="footer-logo">
+        <img src={footer_logo} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
