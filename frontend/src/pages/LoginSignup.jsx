25336f02d7ef608d567e5936295e6eaa6f716eb2
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-17 09:28:58 +0100

    created_loginsign_up_component

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -10,6 +10,14 @@ const LoginSignup = () => {
           <input type="email" placeholder="Email Address" />
           <input type="password" placeholder="Password" />
         </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Log in here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
       </div>
     </div>
   );
