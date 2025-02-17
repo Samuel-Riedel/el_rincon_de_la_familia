29f52091997e0ffb9324ac4d4ff7d9b374a29e99
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-17 08:16:07 +0100

    added_facebook_logo_to_footer_compenent

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Footer.css";
 //import footer_logo from "../assets/logo_big.png";
-import instagram_icon from "../assets/instagram_icon.png";
+//import instagram_icon from "../assets/instagram_icon.png";
 //import pintester_icon from "../assets/pintester_icon.png";
 import whatsapp_icon from "../assets/whatsapp_icon.png";
 import rincon from "../assets/rincon.jpg";
 import paypal_solid from "../assets/cc-paypal-brands-solid.svg";
+import facebook_solid from "../assets/facebook-brands-solid.svg";
 
 const Footer = () => {
   return (
@@ -22,7 +23,7 @@ const Footer = () => {
         </ul>
         <div className="footer-social-icon">
           <div className="footer-icons-container">
-            <img src={instagram_icon} alt="" />
+            <img src={facebook_solid} alt="" className="facebook-logo" />
           </div>
           <div className="footer-icons-container">
             <img src={paypal_solid} alt="" className="paypal-logo" />
