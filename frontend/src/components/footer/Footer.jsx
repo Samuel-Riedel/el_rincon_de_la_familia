b3dd37e68cfe00477a2fff757c45ba0c1d6ba17f
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-17 08:11:52 +0100

    added_paypal_logo_to_footer_compenent

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./Footer.css";
 //import footer_logo from "../assets/logo_big.png";
 import instagram_icon from "../assets/instagram_icon.png";
-import pintester_icon from "../assets/pintester_icon.png";
+//import pintester_icon from "../assets/pintester_icon.png";
 import whatsapp_icon from "../assets/whatsapp_icon.png";
 import rincon from "../assets/rincon.jpg";
+import paypal_solid from "../assets/cc-paypal-brands-solid.svg";
 
 const Footer = () => {
   return (
@@ -24,7 +25,7 @@ const Footer = () => {
             <img src={instagram_icon} alt="" />
           </div>
           <div className="footer-icons-container">
-            <img src={pintester_icon} alt="" />
+            <img src={paypal_solid} alt="" className="paypal-logo" />
           </div>
           <div className="footer-icons-container">
             <img src={whatsapp_icon} alt="" />
