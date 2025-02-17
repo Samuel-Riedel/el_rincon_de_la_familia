8f705cc0d95fab2283fcb5d0cdc302ed735e6c3e
Samuel_Riedel <samuel.riedel@outlook.com>
2025-02-17 09:22:10 +0100

    created_inputs_for_log_in

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const LoginSignup = () => {
-  return <div></div>;
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1> Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSignup;
