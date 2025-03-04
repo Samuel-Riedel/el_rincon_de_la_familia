aff37a0ce776a36b55d5f8e24637df1ce6614b5d
Samuel_Riedel <samuel.riedel@outlook.com>
2025-03-04 13:25:08 +0100

    created_html_for_cs_on_product_display_component

--- frontend/src/components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/components/ProductDisplay/ProductDisplay.jsx
@@ -28,6 +28,36 @@ const ProductDisplay = (props) => {
           <img src={star_dull_icon} alt="" />
           <p>(122)</p>
         </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">
+            ${product.old_price}
+          </div>
+          <div className="productdisplay-right-price-new">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="product-right-description">Light Green Shirt</div>
+        <div className="product-display-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-rightsize">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button> ADD TO CART</button>
+        <p className="productdisplay-right-category">
+          <span>
+            Category: <span>Women, T-Shirt, Croptop</span>
+          </span>
+        </p>
+        <p className="productdisplay-right-category">
+          <span>
+            Tags: <span>Modern, Latest</span>
+          </span>
+        </p>
       </div>
     </div>
   );
