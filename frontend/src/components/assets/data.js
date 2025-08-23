cc3a8da25e2c65e2a5343c945f0e159047620ccf
Samuel_Riedel <samuel.riedel@outlook.com>
2025-08-23 22:49:59 +0200

    changed_images_assets/data_for_popoular_section

--- frontend/src/components/assets/data.js
+++ frontend/src/components/assets/data.js
@@ -1,7 +1,12 @@
 import p1_img from "./mueble_1.1.jpg";
+import p2_img from "./mueble_1.2.jpg";
+import p3_img from "./mueble_1.3.jpg";
+import p4_img from "./mueble_1.5.jpg";
+/*
 import p2_img from "./product_2.png";
 import p3_img from "./product_3.png";
 import p4_img from "./product_4.png";
+*/
 
 let data_product = [
   {
@@ -13,21 +18,21 @@ let data_product = [
   },
   {
     id: 2,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Sofa Chaise Longue Blanco",
     image: p2_img,
     new_price: 85.0,
     old_price: 120.5,
   },
   {
     id: 3,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Conjunto de mesa de comedor con seis sillas",
     image: p3_img,
     new_price: 60.0,
     old_price: 100.5,
   },
   {
     id: 4,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Candelabro Cristal",
     image: p4_img,
     new_price: 100.0,
     old_price: 150.0,
