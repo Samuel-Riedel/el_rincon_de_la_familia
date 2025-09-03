aed80a1dc6b1e5d0df1b3d60be14b123e16e0f9c
Samuel-Riedel <samuel.riedel@outlook.com>
2025-09-03 15:46:47 +0200

    removed_euro_sign_from_lp_on_item_component

--- frontend/src/components/item/item.jsx
+++ frontend/src/components/item/item.jsx
@@ -10,8 +10,14 @@ const Item = (props) => {
       </Link>
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">€{props.new_price}</div>
-        <div className="item-price-old">€{props.old_price}</div>
+        <div className="item-price-new">
+          {/*€ removed euro sign*/}
+          {props.new_price}
+        </div>
+        <div className="item-price-old">
+          {/*€ Removed euro sign*/}
+          {props.old_price}
+        </div>
       </div>
     </div>
   );
