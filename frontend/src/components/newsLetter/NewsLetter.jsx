97840944808a1a4d89d83e543333c17920718d1b
Samuel-Riedel <samuel.riedel@outlook.com>
2025-09-06 21:37:38 +0200

    changed_icon_on_news_letter

--- frontend/src/components/newsLetter/NewsLetter.jsx
+++ frontend/src/components/newsLetter/NewsLetter.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./NewsLetter.css";
+import whatsapp_icon from "../assets/whatsapp_icon.png";
 
 const NewsLetter = () => {
   return (
@@ -7,8 +8,15 @@ const NewsLetter = () => {
       <h1>Obtén información exclusiva en tu correo electrónico.</h1>
       <p>Suscríbete a nuestro boletín y mantente actualizado.</p>
       <div className="subscribe-input-newsletter">
-        <input type="email" placeholder="Tu dirección de correo electrónico" />
-        <button>Suscríbete</button>
+        <div className="whatsappicon-newsletter">
+          <a
+            href="https://wa.me/+34624217879"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={whatsapp_icon} alt="" />
+          </a>
+        </div>
       </div>
     </div>
   );
